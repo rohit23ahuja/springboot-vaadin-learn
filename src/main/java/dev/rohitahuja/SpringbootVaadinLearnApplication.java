fe7d25eab048f873e07664a5f12bafa6bb01638a
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-06-28 18:39:08 +0530

    initial commit

---
+++ src/main/java/dev/rohitahuja/SpringbootVaadinLearnApplication.java
@@ -0,0 +1,13 @@
+package dev.rohitahuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVaadinLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVaadinLearnApplication.class, args);
+	}
+
+}
